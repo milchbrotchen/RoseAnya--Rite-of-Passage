75f192160ce8973ef456cdd61a1b0fd2e9f3c9f6
Joseph Winemberg <42650208+WinembergJo@users.noreply.github.com>
2020-01-11 21:40:06 -0500

    Camera Follow and Camera Collision Scripts made
    A prefab of the new Camera Object can be found in the JosephPrefabs folder in the TechTeam directory.

---
+++ Rosa_TwoStepGame/Assets/Tech Team/Scripts/JosephScripts/CameraCollision_Joseph.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollision_Joseph : MonoBehaviour
+{
+    #region Public
+    public float MinDistance = 1.0f;
+    public float MaxDistance = 4.0f;
+    public float Smooth = 10.0f;
+    public Vector3 DollyDirAdjusted;
+    public float Distance;
+    #endregion
+
+    #region Private
+    private Vector3 DollyDir;
+    #endregion
+
+    void Start()
+    {
+        DollyDir = transform.localPosition.normalized;
+        Distance = transform.localPosition.magnitude;
+    }
+
+    void Update()
+    {
+        Vector3 DesiredCameraPos = transform.parent.TransformPoint(DollyDir * MaxDistance);
+        RaycastHit Hit;
+
+        if(Physics.Linecast(transform.parent.position, DesiredCameraPos, out Hit))
+        {
+            Distance = Mathf.Clamp((Hit.distance * 0.85f), MinDistance, MaxDistance);
+        }
+        else
+        {
+            Distance = MaxDistance;
+        }
+
+        transform.localPosition = Vector3.Lerp(transform.localPosition, DollyDir * Distance, Time.deltaTime * Smooth);
+    }
+}
---
+++ Rosa_TwoStepGame/Assets/Tech Team/Scripts/JosephScripts/CameraFollow_Joseph.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow_Joseph : MonoBehaviour
+{
+    #region Public
+    public float CameraMoveSpeed = 120.0f;
+    public GameObject CameraFollowObject;
+    public float ClampAngleMin = -40.0f;
+    public float ClampAngleMax = 40.0f;
+    public float InputSensitivity = 150.0f;
+    public float CamDistanceXToPlayer;
+    public float CamDistanceYToPlayer;
+    public float CamDistanceZToPlayer;
+    public float MouseX;
+    public float MouseY;
+    public float FinalInputX;
+    public float FinalInputZ;
+    public float SmoothX;
+    public float SmoothY;
+    #endregion
+
+    #region Private
+    private Vector3 FollowPOS;
+    private float RotX = 0.0f;
+    private float RotY = 0.0f;
+    #endregion
+
+    void Start()
+    {
+        //Initializing Variables and Locking/Hiding Cursor
+        Vector3 Rot = transform.localRotation.eulerAngles;
+        RotY = Rot.y;
+        RotX = Rot.x;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        //Set up Gamepad Sticks here
+        MouseX = Input.GetAxis("Mouse X");
+        MouseY = Input.GetAxis("Mouse Y");
+        FinalInputX = MouseX; //Add Stick Input Here
+        FinalInputZ = MouseY; //Add Stick Input Here
+
+        RotY += FinalInputX * InputSensitivity * Time.deltaTime;
+        RotX += FinalInputZ * InputSensitivity * Time.deltaTime;
+
+        RotX = Mathf.Clamp(RotX, ClampAngleMin, ClampAngleMax);
+
+        Quaternion LocalRotation = Quaternion.Euler(RotX, RotY, 0.0f);
+        transform.rotation = LocalRotation;
+    }
+
+    private void LateUpdate()
+    {
+        CameraUpdater();
+    }
+
+    private void CameraUpdater()
+    {
+        Transform Target = CameraFollowObject.transform;
+
+        float step = CameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, Target.position, step);
+    }
+}
