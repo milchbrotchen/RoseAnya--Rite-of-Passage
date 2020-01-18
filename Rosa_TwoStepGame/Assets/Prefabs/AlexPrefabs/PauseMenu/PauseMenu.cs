fb797dc0c3bfe8fb1d5ad4ddf041416eb0b40c3e
Alex <heitzalex@yahoo.com>
2020-01-18 00:18:31 -0500

    Added simple pause menu

---
+++ Rosa_TwoStepGame/Assets/Prefabs/AlexPrefabs/PauseMenu/PauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool IsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.P))
+       {
+           if (IsPaused)
+           {
+               Resume();
+           }
+           else
+           {
+               Pause();
+           }
+       } 
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        IsPaused = true;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Cursor.visible = true;
+    }
+
+
+
+
+
+}
